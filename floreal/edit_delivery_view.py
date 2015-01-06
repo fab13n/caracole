2ccdc78d7a6d4a6b8d540d47d076327857d0cc10
Fabien Fleutot <fabien.fleutot@atmosphere.aero>
2015-01-06 18:44:09 +0100

    take delivery edition POSTs into account

--- floreal/edit_delivery_view.py
+++ floreal/edit_delivery_view.py
@@ -27,20 +27,78 @@ def make_form(delivery):
             'products': _get_products_list(delivery)}
 
 
+def _get_pd_fields(d, prefix, id):
+    """Retrieve form fields representing a product."""
+    fields = ['name', 'price', 'quantity_per_package', 'unit', 'quantity_limit']
+    raw = {f: d["%s%d-%s" % (prefix, id, f)] for f in fields}
+    if not any(f for f in raw.values()):
+        return { 'deleted': True }  # All fields empty means deleted
+    qpp = raw['quantity_per_package']
+    quota = raw['quantity_limit']
+    return {'name': raw['name'],
+            'price': float(raw['price']),
+            'quantity_per_package': int(qpp) if qpp else None,
+            'unit': raw['unit'],
+            'quantity_limit': int(quota) if quota else None,
+            'deleted': "%s%d-deleted" % (prefix, id) in d }
+
+
+def _pd_update(pd, fields):
+    """Update a model object according to form fields."""
+    pd.name = fields['name']
+    pd.price = fields['price']
+    quantity_per_package = fields['quantity_per_package']
+    pd.unit = fields['unit']
+    quantity_limit = fields['quantity_limit']
+
 
 def parse_form(request):
+    """Parse a delivery edition form and update DB accordingly."""
     d = request.POST
-    print(d.__dict__)
+    print(d)
     dv = Delivery.objects.get(pk=int(d['dv-id']))
-    for pd_id in str(d['product_ids']).split(','):
-        pd = Product.objects.get(pk=pd_id)
-        if pd.delivery == dv:
-            if d.get('pd%s-deleted' % pd_id, False):  # Delete previously existing product
-                print "Deleting product %s" % pd_id
-            else:  # Update product
-                print "Updating product %s" % pd_id
+
+    # Edit delivery name and state
+    dv.name = d['dv-name']
+    dv.state = d['dv-state']
+    dv.save()
+
+    # Parse preexisting products
+    product_ids_string = str(d['product_ids'])
+    if product_ids_string:
+        for pd_id in product_ids_string.split(','):
+            pd = Product.objects.get(pk=pd_id)
+            fields = _get_pd_fields(d, 'pd', int(pd_id))
+            if pd.delivery == dv:
+                if fields['deleted']:  # Delete previously existing product
+                    print "Deleting product %s" % pd_id
+                    pd.delete()
+                else:  # Update product
+                    print "Updating product %s" % pd_id
+                    _pd_update(pd, fields)
+                    pd.save(force_update=True)
+            else:  # From another delivery
+                if fields['deleted']:  # Don't import product
+                    print "Ignoring past product %s" % pd_id
+                    pass
+                else:  # Import product copy from other delivery
+                    print "Importing past product %s" % pd_id
+                    _pd_update(pd, fields)
+                    pd.delivery = dv
+                    pd.save(force_create=True)
+
+    # Parse products created from blank lines
+    for i in range(int(d['n_blanks'])):
+        # Create new product
+        fields =  _get_pd_fields(d, 'blank', i)
+        if fields['deleted']:
+            print "Blank field #%d deleted/empty" % i
         else:
-            if d.get('pd%s-deleted' % pd_id, False):  # Import product copy from other delivery
-                print "Importing past product %s" % pd_id
-            else:  # Don't import product
-                print "Ignoring past product %s" % pd_id
+            print "Adding product from blank line #%d" % i
+            pd = Product.objects.create(name=fields['name'],
+                                        price=fields['price'],
+                                        quantity_per_package=fields['quantity_per_package'],
+                                        quantity_limit=fields['quantity_limit'],
+                                        unit=fields['unit'],
+                                        delivery=dv)
+            pd.save()
