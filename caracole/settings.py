a22ec8554db3e86e751df65d114dbe982a46a83e
Fabien Fleutot <fleutot@gmail.com>
2015-01-15 23:02:05 +0100

    prod setup, WiP

--- caracole/settings.py
+++ caracole/settings.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-# -*- coding: utf8 -*-
+# -*- coding: utf-8 -*-
 
 """
 Django settings for floreal project.
@@ -78,7 +78,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/caracole/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "floreal", "static"),
